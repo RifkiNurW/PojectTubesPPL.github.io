1cb879fef512ac1a7eafaeadc04e6ed2d8ef08ee
RifkiNurW <rifkinurw00@gmail.com>
2023-12-15 09:55:07 +0700

    Project

---
+++ register.php
@@ -0,0 +1,14 @@
+<?php
+require 'koneksi.php';
+$email = $_POST["email"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$quary_sql = "INSERT INTO tbl_users (username, email, password)
+            VALUE ('$username', '$email', '$password')";
+
+if(mysqli_query($conn, $quary_sql)) {
+    header("location: login.html");
+} else {
+    echo "pendaftaran Gagal : " . mysqli_error($conn);
+}
